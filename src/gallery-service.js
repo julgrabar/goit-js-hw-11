4330e59ec35b0c8020ba8b628447e153b538d82b
julgrabar <45499113+julgrabar@users.noreply.github.com>
2022-01-19 12:05:03 +0200

    Update gallery-service.js

--- src/gallery-service.js
+++ src/gallery-service.js
@@ -10,7 +10,6 @@ export default class GalleryService{
     async findImages(){
         this.page+=1;
         const response = await axios.get(`https://pixabay.com/api/?key=25268338-c83be630200395a9374b8c8e0&q=${this.searchRequest}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`);
-        // checkLostImages(response.data);
         return response.data;   
     }   
 }
